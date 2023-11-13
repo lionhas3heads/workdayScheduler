6e5d094c1bee62634c63a987ae404ee0ae922470
Leo Quijano II <leoquijano@Leos-MacBook-Pro-2.local>
2023-11-12 17:14:53 -0800

    Worked on JS for schedule.'

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+const currentDayEl = document.querySelector('#currentDay');
+const timeBlockWrapperEl = document.querySelector('#time-block-wrapper');
+currentDayEl.textContent = dayjs().format('MM-DD-YYYY');
+
+let currentHour = dayjs().hour();
+
+for(i = 0; i <=9; i++) {
+  if (currentHour == timeBlockWrapperEl.children[i].id.slice(5)) {
+    timeBlockWrapperEl.children[i].classList.add('present'); 
+  } else if (currentHour > timeBlockWrapperEl.children[i].id.slice(5)) {
+    timeBlockWrapperEl.children[i].classList.add('past');
+  } else {
+    timeBlockWrapperEl.children[i].classList.add('future');
+  }
+}
+
+
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +33,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
